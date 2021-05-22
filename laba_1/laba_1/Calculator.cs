c46124388bfbd18b27a2f16b6eb4adc16b286caf
Daria <79308800+DariaKovalenk0@users.noreply.github.com>
2021-05-22 17:21:49 +0300

    Create Calculator.cs

---
+++ laba_1/laba_1/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Calculator
+{
+    private readonly ILogger _logger;
+
+    public Calculator(ILogger logger)
+    {
+        _logger = logger;
+    }
+
+    public double Der_tg(double x)
+    {
+        var result = 1 / Math.Pow(Math.Cos(x), 2);
+        _logger.Log(result);
+        return result;
+    }
+
+    public double Der_ln(double x)
+    {
+        var result = 1 / x;
+        _logger.Log(result);
+        return result;
+    }
+
+    public double Der_exp(double x)
+    {
+        var result = Math.Exp(x);
+        _logger.Log(result);
+        return result;
+    }
+}
